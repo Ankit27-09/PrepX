1ef6b8c570a172a0aa0348c53980dd531f31d706
Ankit27-09 <ankit27092004@gmail.com>
2026-01-11 22:19:59 +0530

    Refactor main.tsx to integrate Redux and React Query providers, replacing App component with RouterProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './router.tsx'
+import { Provider } from "react-redux";
+import { store } from './store/store.ts';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 5000,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </Provider>
+);
